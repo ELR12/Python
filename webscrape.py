218f76897285c9ec97fa5f97b8487d7f399d9588
Ethan Roberts <erobert2@my.athens.edu>
2019-12-15 10:52:35 -0600

    Added functionality to loop through the pages of the webpage

--- webscrape.py
+++ webscrape.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 from csv import writer
 
 
-url = 'https://www.newegg.com/p/pl?Submit=ENE&IsNodeId=1&N=100160979%20600557170%20601305587&cm_sp=CAT_Monitors_2-_-VisNav-_-4K-42-Monitors_2'
+url = 'https://www.newegg.com/p/pl?N=100160979%20600557170%20601305587&page=1'
 response = requests.get(url)  # must read 200 which means successfully connected to website
 src = response.content  #contains the source-code of the website
 soup = BeautifulSoup(src, 'html.parser') 
@@ -20,13 +20,21 @@ with open('monitorPrices.csv', 'w') as csv_file:    #write to csv
 	fileHeaders = ['Monitor Info', 'Regular Price', 'Current Price']
 	csv_writer.writerow(fileHeaders)
 
-	for item in soup.select('.item-info'):  #using soup.select, could've used soup.findAll
-		itemName = item.find('a', class_='item-title').get_text()
-		dollarAmount = item.find('span', class_='price-current-label').findNext('strong').get_text()
-		cents = item.find('span', class_='price-current-label').findNext('sup').get_text()
-		newPrice = dollarAmount + cents
-		if (item.find('span', class_='price-was-data')):
-			regPrice = item.find('span', class_='price-was-data').get_text()
-		else:
-			regPrice = newPrice
-		csv_writer.writerow([itemName, regPrice, newPrice])
+	#loop through pages 1 - 3 of the website and scrape data
+	for i in range(1, 3):
+		url = 'https://www.newegg.com/p/pl?N=100160979%20600557170%20601305587&page={}'.format(i)
+		response = requests.get(url)  # must read 200 which means successfully connected to website
+		src = response.content  #contains the source-code of the website
+		soup = BeautifulSoup(src, 'html.parser') 
+        
+		#loop through the current page your on
+		for item in soup.select('.item-info'):  #using soup.select, could've used soup.findAll
+			itemName = item.find('a', class_='item-title').get_text()
+			dollarAmount = item.find('span', class_='price-current-label').findNext('strong').get_text()
+			cents = item.find('span', class_='price-current-label').findNext('sup').get_text()
+			newPrice = dollarAmount + cents
+			if (item.find('span', class_='price-was-data')):
+				regPrice = item.find('span', class_='price-was-data').get_text()
+			else:
+				regPrice = newPrice
+			csv_writer.writerow([itemName, regPrice, newPrice])
